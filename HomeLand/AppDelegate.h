060b49932e60b935774d04f629cf2ef449694aef
hanchao <hanchao0123@hotmail.com>
2014-03-03 11:54:35 +0800

    init

---
+++ HomeLand/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HomeLand
+//
+//  Created by chao han on 14-3-3.
+//  Copyright (c) 2014年 chao han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HomeLand/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  HomeLand
+//
+//  Created by chao han on 14-3-3.
+//  Copyright (c) 2014年 chao han. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
